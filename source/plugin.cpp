382bf4b78f25668804068cf91eb9e9a478f39f8f
Raphael <64648134+RaphaelIT7@users.noreply.github.com>
2023-07-12 23:41:51 +0200

    Updated Plugin.

--- source/plugin.cpp
+++ source/plugin.cpp
@@ -1,112 +1,56 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
-//
-// Purpose: 
-//
-// $NoKeywords: $
-//
-//===========================================================================//
-
-#include <GarrysMod/InterfacePointers.hpp>
-#include "engine/iserverplugin.h"
-#include "memalloc.h"
-
-// useful helper func
-#ifndef GAME_DLL
-inline bool FStrEq(const char *sz1, const char *sz2)
-{
-	return(Q_stricmp(sz1, sz2) == 0);
-}
-#endif
-//---------------------------------------------------------------------------------
-// Purpose: a sample 3rd party plugin class
-//---------------------------------------------------------------------------------
-class CEmptyServerPlugin: public IServerPluginCallbacks
-{
-public:
-	CEmptyServerPlugin();
-	~CEmptyServerPlugin();
-
-	// IServerPluginCallbacks methods
-	virtual bool			Load(	CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory );
-	virtual void			Unload( void );
-	virtual void			Pause( void );
-	virtual void			UnPause( void );
-	virtual const char     *GetPluginDescription( void );      
-	virtual void			LevelInit( char const *pMapName );
-	virtual void			ServerActivate( edict_t *pEdictList, int edictCount, int clientMax );
-	virtual void			GameFrame( bool simulating );
-	virtual void			LevelShutdown( void );
-	virtual void			ClientActive( edict_t *pEntity );
-	virtual void			ClientDisconnect( edict_t *pEntity );
-	virtual void			ClientPutInServer( edict_t *pEntity, char const *playername );
-	virtual void			SetCommandClient( int index );
-	virtual void			ClientSettingsChanged( edict_t *pEdict );
-	virtual PLUGIN_RESULT	ClientConnect( bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen );
-	virtual PLUGIN_RESULT	ClientCommand( edict_t *pEntity, const CCommand &args );
-	virtual PLUGIN_RESULT	NetworkIDValidated( const char *pszUserName, const char *pszNetworkID );
-	virtual void			OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue );
-	virtual void			OnEdictAllocated( edict_t *edict );
-	virtual void			OnEdictFreed( const edict_t *edict  );	
-
-	virtual int GetCommandIndex() { return m_iClientCommandIndex; }
-private:
-	int m_iClientCommandIndex;
-};
-
+#include "plugin.h"
 
 // 
 // The plugin is a static singleton that is exported as an interface
 //
-CEmptyServerPlugin g_EmtpyServerPlugin;
-EXPOSE_SINGLE_INTERFACE_GLOBALVAR(CEmptyServerPlugin, IServerPluginCallbacks, INTERFACEVERSION_ISERVERPLUGINCALLBACKS, g_EmtpyServerPlugin );
+CServerPlugin g_EmtpyServerPlugin;
+EXPOSE_SINGLE_INTERFACE_GLOBALVAR(CServerPlugin, IServerPluginCallbacks, INTERFACEVERSION_ISERVERPLUGINCALLBACKS, g_EmtpyServerPlugin );
 
 //---------------------------------------------------------------------------------
 // Purpose: constructor/destructor
 //---------------------------------------------------------------------------------
-CEmptyServerPlugin::CEmptyServerPlugin()
+CServerPlugin::CServerPlugin()
 {
 	m_iClientCommandIndex = 0;
 }
 
-CEmptyServerPlugin::~CEmptyServerPlugin()
+CServerPlugin::~CServerPlugin()
 {
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called when the plugin is loaded, load the interface we need from the engine
 //---------------------------------------------------------------------------------
-bool CEmptyServerPlugin::Load(	CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory )
+bool CServerPlugin::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory)
 {
-	Msg("Hello from a plugin\n");
-
 	return true;
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called when the plugin is unloaded (turned off)
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::Unload( void )
+void CServerPlugin::Unload(void)
 {
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called when the plugin is paused (i.e should stop running but isn't unloaded)
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::Pause( void )
+void CServerPlugin::Pause(void)
 {
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called when the plugin is unpaused (i.e should start executing again)
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::UnPause( void )
+void CServerPlugin::UnPause(void)
 {
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: the name of this plugin, returned in "plugin_print" command
 //---------------------------------------------------------------------------------
-const char *CEmptyServerPlugin::GetPluginDescription( void )
+const char * CServerPlugin::GetPluginDescription(void)
 {
 	return "Emtpy-Plugin V2, Valve";
 }
@@ -114,70 +58,102 @@ const char *CEmptyServerPlugin::GetPluginDescription( void )
 //---------------------------------------------------------------------------------
 // Purpose: called on level start
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::LevelInit( char const *pMapName )
+void CServerPlugin::LevelInit(char const *pMapName)
 {
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called after LUA Initialized.
+//---------------------------------------------------------------------------------
+bool CServerPlugin::LuaInit()
+{
+	LUA = GetRealm(GarrysMod::Lua::State::SERVER);
+	if (LUA == nullptr) {
+		Msg("Failed to initialize Server\n");
+		return false;
+	}
+
+	// Add your code below.
+	LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+		LUA->GetField(-1, "print");
+			LUA->PushString("Hello World from a Plugin :D");
+			LUA->Call(1, 0);
+	LUA->Pop();
+
+	return true;
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called just before LUA is shutdown.
+//---------------------------------------------------------------------------------
+void CServerPlugin::LuaShutdown()
+{
+	LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+		LUA->GetField(-1, "print");
+			LUA->PushString("Bye World from a Plugin :D");
+			LUA->Call(1, 0);
+	LUA->Pop();
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called on level start, when the server is ready to accept client connections
 //		edictCount is the number of entities in the level, clientMax is the max client count
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::ServerActivate( edict_t *pEdictList, int edictCount, int clientMax )
+void CServerPlugin::ServerActivate(edict_t *pEdictList, int edictCount, int clientMax)
 {
+	if (!lua_init) {
+		lua_init = LuaInit();
+	}
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called once per server frame, do recurring work here (like checking for timeouts)
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::GameFrame( bool simulating )
+void CServerPlugin::GameFrame(bool simulating)
 {
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called on level end (as the server is shutting down or going to a new map)
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::LevelShutdown( void ) // !!!!this can get called multiple times per map change
+void CServerPlugin::LevelShutdown(void) // !!!!this can get called multiple times per map change
 {
+	if (lua_init) {
+		LuaShutdown();
+		lua_init = false;
+	}
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called when a client spawns into a server (i.e as they begin to play)
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::ClientActive( edict_t *pEntity )
+void CServerPlugin::ClientActive(edict_t *pEntity)
 {
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called when a client leaves a server (or is timed out)
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::ClientDisconnect( edict_t *pEntity )
+void CServerPlugin::ClientDisconnect(edict_t *pEntity)
 {
 }
 
 //---------------------------------------------------------------------------------
-// Purpose: called on 
+// Purpose: called when a client spawns?
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::ClientPutInServer( edict_t *pEntity, char const *playername )
+void CServerPlugin::ClientPutInServer(edict_t *pEntity, char const *playername)
 {
-	KeyValues *kv = new KeyValues( "msg" );
-	kv->SetString( "title", "Hello" );
-	kv->SetString( "msg", "Hello there" );
-	kv->SetColor( "color", Color( 255, 0, 0, 255 ));
-	kv->SetInt( "level", 5);
-	kv->SetInt( "time", 10);
-	//helpers->CreateMessage( pEntity, DIALOG_MSG, kv, this );
-	kv->deleteThis();
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called on level start
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::SetCommandClient( int index )
+void CServerPlugin::SetCommandClient(int index)
 {
 	m_iClientCommandIndex = index;
 }
 
-void ClientPrint( edict_t *pEdict, char *format, ... )
+void ClientPrint(edict_t *pEdict, char *format, ...)
 {
 	va_list		argptr;
 	static char		string[1024];
@@ -189,14 +165,14 @@ void ClientPrint( edict_t *pEdict, char *format, ... )
 //---------------------------------------------------------------------------------
 // Purpose: called on level start
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::ClientSettingsChanged( edict_t *pEdict )
+void CServerPlugin::ClientSettingsChanged(edict_t *pEdict)
 {
 }
 
 //---------------------------------------------------------------------------------
 // Purpose: called when a client joins a server
 //---------------------------------------------------------------------------------
-PLUGIN_RESULT CEmptyServerPlugin::ClientConnect( bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen )
+PLUGIN_RESULT CServerPlugin::ClientConnect(bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen)
 {
 	return PLUGIN_CONTINUE;
 }
@@ -204,7 +180,7 @@ PLUGIN_RESULT CEmptyServerPlugin::ClientConnect( bool *bAllowConnect, edict_t *p
 //---------------------------------------------------------------------------------
 // Purpose: called when a client types in a command (only a subset of commands however, not CON_COMMAND's)
 //---------------------------------------------------------------------------------
-PLUGIN_RESULT CEmptyServerPlugin::ClientCommand( edict_t *pEntity, const CCommand &args )
+PLUGIN_RESULT CServerPlugin::ClientCommand(edict_t *pEntity, const CCommand &args)
 {
 	return PLUGIN_CONTINUE;
 }
@@ -212,7 +188,7 @@ PLUGIN_RESULT CEmptyServerPlugin::ClientCommand( edict_t *pEntity, const CComman
 //---------------------------------------------------------------------------------
 // Purpose: called when a client is authenticated
 //---------------------------------------------------------------------------------
-PLUGIN_RESULT CEmptyServerPlugin::NetworkIDValidated( const char *pszUserName, const char *pszNetworkID )
+PLUGIN_RESULT CServerPlugin::NetworkIDValidated(const char *pszUserName, const char *pszNetworkID)
 {
 	return PLUGIN_CONTINUE;
 }
@@ -220,13 +196,14 @@ PLUGIN_RESULT CEmptyServerPlugin::NetworkIDValidated( const char *pszUserName, c
 //---------------------------------------------------------------------------------
 // Purpose: called when a cvar value query is finished
 //---------------------------------------------------------------------------------
-void CEmptyServerPlugin::OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue )
-{
-	Msg( "Cvar query (cookie: %d, status: %d) - name: %s, value: %s\n", iCookie, eStatus, pCvarName, pCvarValue );
-}
-void CEmptyServerPlugin::OnEdictAllocated( edict_t *edict )
+void CServerPlugin::OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue)
 {
 }
-void CEmptyServerPlugin::OnEdictFreed( const edict_t *edict  )
+
+void CServerPlugin::OnEdictAllocated(edict_t *edict)
+{
+}
+
+void CServerPlugin::OnEdictFreed(const edict_t *edict)
 {
 }
